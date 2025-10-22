d45c19782cba38224d8b17824be1152fd14c2e71
kim254ke <kmntony1@gmail.com>
2025-10-22 16:16:16 +0300

    Vercel Fix: Removed Tailwind Vite plugin and forcing package-lock sync

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+// REMOVED: import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  // PostCSS will automatically find and run Tailwind
+  plugins: [react()], 
 });
